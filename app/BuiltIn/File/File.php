033b6764ee4c86e419a65e83954c78af8c67b3e1
AngelNovo <angelnovo15@gmail.com>
2022-04-26 20:11:27 +0200

    Read file and get last access to a file aded to File::class

--- app/BuiltIn/File/File.php
+++ app/BuiltIn/File/File.php
@@ -10,6 +10,18 @@ class File{
         fwrite($file, $text);
         fclose($file);
     }
+
+    public static function read(string $route): string
+    {
+        $completeRoute = "{$_SERVER['DOCUMENT_ROOT']}/../$route";
+        return file_get_contents($completeRoute);
+    }
+
+    public static function lastAccess(string $route, string $format)
+    {
+        $completeRoute = "{$_SERVER['DOCUMENT_ROOT']}/../$route";
+        return date($format, fileatime($completeRoute));
+    }
 }
 
 ?>
